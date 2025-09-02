9cd203a8c62dbbf4adfc2593918136420c04590e
tungerle <taviungerleider@gmail.com>
2025-09-02 14:31:06 -0400

    echo

--- src/main.cpp
+++ src/main.cpp
@@ -7,18 +7,18 @@ int main() {
   std::cerr << std::unitbuf;
 
   while (true) {
-
   std::cout << "$ ";
-
   std::string input;
   std::getline(std::cin, input);
   if (input == "exit 0"){
     break;
   }
-  if (input.starts_with("echo", 0) == 0){
-    std::cout <<input.substr(5) <<std::endl;
+  else if (input.compare(0, 5, "echo ") == 0){
+    std::cout <<input.substr(6) <<std::endl;
   }
+  else {
   std::cout << input << ": command not found" <<std::endl;
   }
+}
   return 0;
 }
