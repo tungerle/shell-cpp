5fbb68306585ffce40dc69f878668522ce6ba4c4
tungerle <taviungerleider@gmail.com>
2025-09-02 14:50:19 -0400

    type

--- src/main.cpp
+++ src/main.cpp
@@ -10,15 +10,29 @@ int main() {
   std::cout << "$ ";
   std::string input;
   std::getline(std::cin, input);
+
   if (input == "exit 0"){
     break;
   }
+
   else if (input.compare(0, 5, "echo ") == 0){
     std::cout <<input.substr(5) <<std::endl;
   }
+
+  else if (input.compare(0, 5, "type") == 0) {
+    if (input == "type echo") {
+      std::cout << "echo is a shell builtin" <<std::endl;
+    }
+    else if (input == "type exit"){
+      std::cout<< "exit is a shell builtin" <<std::endl;
+    }
+  }
+
   else {
   std::cout << input << ": command not found" <<std::endl;
   }
-}
-  return 0;
+
+} // end While loop
+
+return 0;
 }
