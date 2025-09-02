c15e7b81c86e243209b0776875a5820e3b4f73ae
tungerle <taviungerleider@gmail.com>
2025-09-02 14:57:03 -0400

    type

--- src/main.cpp
+++ src/main.cpp
@@ -11,14 +11,15 @@ int main() {
   std::string input;
   std::getline(std::cin, input);
 
+  // exit
   if (input == "exit 0"){
     break;
   }
-
+  // echo
   else if (input.compare(0, 5, "echo ") == 0){
     std::cout <<input.substr(5) <<std::endl;
   }
-
+  // type
   else if (input.compare(0, 4, "type") == 0) {
     if (input == "type echo") {
       std::cout << "echo is a shell builtin" <<std::endl;
@@ -29,11 +30,15 @@ int main() {
     else if (input == "type type"){
       std::cout<< "type is a shell builtin" <<std::endl;
     }
-
+    else {
+      std::cout << "invalid_command: not found" << std::endl;
+    }
+  }
+  // invalid command
   else {
   std::cout << input << ": command not found" <<std::endl;
     }
-  } 
+   
 } // end while loop
 return 0;
 }
