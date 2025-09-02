adaa22550b0c048e9cd351fef881ce4b8ef6f4f9
tungerle <taviungerleider@gmail.com>
2025-09-02 13:17:04 -0400

    repl

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,12 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-
+  while (true) {
 
   std::cout << "$ ";
 
   std::string input;
   std::getline(std::cin, input);
   std::cout << input << ": command not found" <<std::endl;
+  }
 }
